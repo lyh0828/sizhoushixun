73a55332a62c74de76cbc6280a7bcd581ade18af
‘LiuYaHong’ <1980549412@qq.com>
2022-06-16 10:47:31 +0800

    删除了内容

--- src/router.js
+++ src/router.js
@@ -4,33 +4,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router';
 //导入底部导航栏的三个组件
 import HomeContainer from './components/tabbar/HomeContainer.vue';        //1.首页
-import ShopcartContainer from './components/tabbar/ShopcartContainer.vue';//2.购物车
-import MytbContainer from './components/tabbar/MytbContainer.vue';        //3.我的淘宝
-import GoodsList from './components/goods/GoodsList.vue';                 //1.1首页--天猫--商品列表页
-import GoodsInfo from './components/goods/GoodsInfo.vue';                 //1.1首页--天猫--商品详情页
-import NewsList from './components/news/NewsList.vue';                    //1.2首页--新闻--新闻列表页
-import NewsInfo from './components/news/NewsInfo.vue';                    //1.2首页--新闻--新闻详情页
-import NewsGetcomments from './components/news/NewsGetcomments.vue';      //1.2首页--新闻--新闻详情页--评论
-import SearchGetprodlist from './components/search/SearchGetprodlist.vue';//1.3首页--搜索
-import GetImgcategory from './components/Share/GetImgcategory.vue';       //1.1首页--图片列表
-import GetImages from './components/Share/GetImages.vue';                 //1.1首页--图片列表--图片详情
+import InfoContainer from './components/tabbar/InfoContainer.vue';        //2.发布信息
+import MyContainer from './components/tabbar/MyContainer.vue';            //3.我的
 //通过Vue使用路由（手动安装）
 Vue.use(VueRouter)
 //创建路由对象
 const router = new VueRouter({
     routes:[//匹配路由规则
-       {path:'/',redirect:'/home'},                     //0.默认
-       {path:'/home',component:HomeContainer},          //1.首页
-       {path:'/shopcart',component:ShopcartContainer},  //2.购物车
-       {path:'/mytaobao',component:MytbContainer},      //3.我的淘宝
-       {path:'/home/goodslist',component:GoodsList},    //1.1首页--天猫--商品列表页
-       {path:'/home/goodsinfo/:id',component:GoodsInfo},//1.1首页--天猫--商品详情页
-       {path:'/home/newslist',component:NewsList},      //1.2首页--新闻--新闻列表页
-       {path:'/home/newsinfo/:id',component:NewsInfo},   //1.2首页--新闻--新闻详情页
-       {path:'/home/newsgetcomments/:id',component:NewsGetcomments},//1.2.1首页--新闻--新闻详情页--评论页
-       {path:'/home/searchgetprodlist',component:SearchGetprodlist},//1.3首页--搜索
-       {path:'/home/getimgcategory',component:GetImgcategory},    //1.1首页--图片列表
-       {path:'/home/getimages/:id',component:GetImages},    //1.1首页--图片列表--图片详情
+       {path:'/',redirect:'/home'},                 //0.默认
+       {path:'/home',component:HomeContainer},      //1.首页
+       {path:'/info',component:InfoContainer},      //2.发布信息
+       {path:'/my',component:MyContainer}           //3.我的
     ],
     linkActiveClass:'mui-active'
 })
